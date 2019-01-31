7eb4e74fed7c366abe62ad58f76b58cec8e5b0d7
Daniel Bailey <daniel4423@gmail.com>
2019-01-31 21:00:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function johnLennonFacts(facts) {
   var maxLength = facts.length
   var index = 0;
   while (index <= maxLength) {
-    facts[index].concat('!!!');
+    facts[index] = facts[index] + '!!!';
     index = index + 1;
   }
   return facts;
