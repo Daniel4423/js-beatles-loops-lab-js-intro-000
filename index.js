5836abe0fa0c3c082ae43ecf680a7109715ac68f
Daniel Bailey <daniel4423@gmail.com>
2019-01-31 21:00:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function theBeatlesPlay(musicians, instruments) {
 function johnLennonFacts(facts) {
   var maxLength = facts.length
   var index = 0;
-  while (index <= maxLength) {
+  while (index < maxLength) {
     facts[index] = facts[index] + '!!!';
     index = index + 1;
   }
