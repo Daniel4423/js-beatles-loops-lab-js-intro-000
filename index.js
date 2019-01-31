fefb7349394668ad5c04b17f924bbe17966205a6
Daniel Bailey <daniel4423@gmail.com>
2019-01-31 20:34:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var eachBeatlePlays = [];
+  for (let i = 0; i < 5; i++) {
+    eachBeatlePlays[i] = `${musicians[i]} plays ${instruments[i]}`; 
+  }
+  return eachBeatlePlays;
+}
